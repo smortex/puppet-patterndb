e8dfea7a65023c62826831d188f6d318a1d9cb7d
Fabien Wernli <cpan@faxm0dem.org>
2014-03-21 15:24:13 +0100

    test

--- manifests/pdb/update.pp
+++ manifests/pdb/update.pp
@@ -10,7 +10,7 @@ class syslogng::pdb::update (
 	ensure_resource ( 'exec', 'syslogng::pdb::merge',
 		{ 
 			command => "pdbtool merge -r --glob \\*.pdb -D $::syslogng::pdb::pdb_dir -p ${::syslogng::temp_dir}/patterndb.xml",
-			path => [ "/usr/bin", "/bin" ],
+			path => "/bin:/usr/bin",
 			logoutput => true,
 			refreshonly => true,
 			notify      => Exec['syslogng::pdb::deploy']
@@ -19,8 +19,8 @@ class syslogng::pdb::update (
 
 	ensure_resource ( 'exec', 'syslogng::pdb::deploy',
 		{ 
+			path => "/bin:/usr/bin",
 			command => "cp ${::syslogng::temp_dir}/patterndb.xml ${::syslogng::base_dir}/var/lib/syslog-ng/",
-			path => [ "/usr/bin", "/bin" ],
 			# pdbtool writes version 3 see bug on github
 			#onlyif  => "/usr/bin/pdbtool --validate test ${::syslogng::temp_dir}/patterndb.xml",
 			onlyif  => "pdbtool test ${::syslogng::temp_dir}/patterndb.xml",
