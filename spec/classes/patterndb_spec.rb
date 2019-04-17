0ce508aad91c19c7c47536f4dfb524f5989a59b5
Fᴀʙɪᴇɴ Wᴇʀɴʟɪ <wernli@in2p3.fr>
2019-04-17 14:11:00 +0200

    fix tests to recent rspec/pdk/etc.

--- spec/classes/patterndb_spec.rb
+++ spec/classes/patterndb_spec.rb
@@ -1,42 +1,40 @@
 require 'spec_helper'
 
-oses_specs = @oses_specs
-
 describe 'patterndb', :type => 'class' do
-  oses_specs.each do |osname, specs|
-    context "#{osname} OS without package_name" do
-      let :facts do {
-          :osfamily        => specs[:osfamily],
-          :operatingsystem => specs[:operatingsystem]
-      }
+  on_supported_os.each do |os, facts|
+    context "#{os} without package_name" do
+      let (:facts) { facts }
+      it { is_expected.to compile.with_all_deps }
+      case facts[:osfamily]
+      when 'Debian'
+        it { should contain_package('syslog-ng-core')}
+      when 'RedHat'
+        it { should contain_package('syslog-ng')}
       end
-      it {
-        should contain_package(specs[:syslog_ng_package])
-      }
     end
-    context "#{osname} OS without managing package" do
+    context "#{os} OS without managing package" do
+      let (:facts) { facts }
       let :params do
         { :manage_package => false }
       end
-      it {
-        should_not contain_package(specs[:syslog_ng_package])
-      }
+      case facts[:osfamily]
+      when 'Debian'
+        it { should_not contain_package('syslog-ng-core')}
+      when 'RedHat'
+        it { should_not contain_package('syslog-ng')}
+      end
     end
   end
   context "Unsupported OS without package_name" do
     let :facts do
       { :osfamily => 'UnsupportedOne' }
     end
-    it {
-      expect {should raise_error(Puppet::Error)}
-    }
+    it { expect {should raise_error(Puppet::Error)}}
   end
   context "Any OS with a package name" do
     let :params do
       { :package_name => 'othersyslogngpackagename' }
     end
-    it {
-      should contain_package('othersyslogngpackagename')
-    }
+    it { should contain_package('othersyslogngpackagename') }
   end
 end
